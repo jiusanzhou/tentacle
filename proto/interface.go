9b68da09ab005bde652e4b9d9f5cad493cedd9ec
jiusanzhou <jsz3@live.com>
2017-05-15 16:54:45 +0800

    ...

--- proto/interface.go
+++ proto/interface.go
@@ -1,1 +1,10 @@
 package proto
+
+import (
+	"github.com/jiusanzhou/tentacle/conn"
+)
+
+type Protocol interface {
+	GetName() string
+	WrapConn(conn.Conn, interface{}) conn.Conn
+}
--- proto/tcp.go
+++ proto/tcp.go
@@ -1,1 +1,15 @@
 package proto
+
+import "github.com/jiusanzhou/tentacle/conn"
+
+type Tcp struct{}
+
+func NewTcp() *Tcp {
+	return new(Tcp)
+}
+
+func (h *Tcp) GetName() string { return "tcp" }
+
+func (h *Tcp) WrapConn(c conn.Conn, ctx interface{}) conn.Conn {
+	return c
+}
