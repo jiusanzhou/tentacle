b9f2a6a916fa06a955fc79227bbd6ac6498ff139
jiusanzhou <jsz3@live.com>
2017-06-03 11:40:43 +0800

    ...

--- main/test/a.go
+++ main/test/a.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"net/http"
+	"net/http/httputil"
 	"os"
-	"time"
 	"os/signal"
 	"syscall"
+	"time"
 )
 
 type Options struct {
@@ -51,11 +53,11 @@ func listen(addr string) {
 
 func dial(addr string) {
 	conn, e := net.Dial("tcp", addr)
-	if e!=nil{
+	if e != nil {
 		log.Fatalln("Dial error", e)
 	}
 	buf := []byte("Hello World")
-	reap := time.NewTicker(5*time.Second)
+	reap := time.NewTicker(5 * time.Second)
 	ch := make(chan os.Signal, 1)
 	signal.Notify(ch, os.Interrupt, syscall.SIGTERM)
 	for {
@@ -92,4 +94,41 @@ func main() {
 		fmt.Println("Unknow command ", opts.cmd)
 		os.Exit(0)
 	}
+}
+
+type Handler struct{}
+
+func removeProxyHeaders(r *http.Request) {
+	r.RequestURI = "" // this must be reset when serving a request with the client
+	// If no Accept-Encoding header exists, Transport will add the headers it can accept
+	// and would wrap the response body with the relevant reader.
+	r.Header.Del("Accept-Encoding")
+	// curl can add that, see
+	// https://jdebp.eu./FGA/web-proxy-connection-header.html
+	r.Header.Del("Proxy-Connection")
+	r.Header.Del("Proxy-Authenticate")
+	r.Header.Del("Proxy-Authorization")
+	// Connection, Authenticate and Authorization are single hop Header:
+	// http://www.w3.org/Protocols/rfc2616/rfc2616.txt
+	// 14.10 Connection
+	//   The Connection general-header field allows the sender to specify
+	//   options that are desired for that particular connection and MUST NOT
+	//   be communicated by proxies over further connections.
+	r.Header.Del("Connection")
+}
+
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "CONNECT" {
+
+	} else {
+		removeProxyHeaders(r)
+		b, _ := httputil.DumpRequest(r, true)
+		r.Body.Close()
+		fmt.Println(string(b))
+	}
+}
+
+func Main() {
+	h := &Handler{}
+	http.ListenAndServe(":8080", h)
 }
